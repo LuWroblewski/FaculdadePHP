4b382ae2cbe5a82b68f4d88ead7ab938a9d90350
Small-Lexy <92353062+Small-Lexy@users.noreply.github.com>
2022-09-05 23:36:02 -0300

    Update view.php

--- FaculdadePHP/view.php
+++ FaculdadePHP/view.php
@@ -13,10 +13,11 @@
     <div>
         <section class="containerTabela2">
             <?php
-            $endereço = '167.99.252.245';
-            $usuario = 'BSI_E4';
-            $senha = 'bsi@e42022';
-            $bd = 'BSI_E4_LU';
+            $endereço = '';
+            $usuario = '';
+            $senha = '';
+            $bd = '';
+
 
             $connect = mysqli_connect($endereço, $usuario, $senha, $bd);
             $qry = "SELECT * FROM Produtos";
